f5c6ddac93b6eb6d036946115c0cf93a91f058b2
m-tetsuya <matsuyama.tetsuya@gmail.com>
2014-04-19 00:43:23 +0900

    first commit

---
+++ test/clojure_storm_sample/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-storm-sample.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-storm-sample.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
